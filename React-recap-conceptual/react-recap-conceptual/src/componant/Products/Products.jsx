79cc5270c24e5a3bcbe7267608ed64498972eb8e
wasifaislam88 <wasifaislam88@gmail.com>
2025-02-01 22:56:16 +0600

    usestate and useEffect

---
+++ React-recap-conceptual/react-recap-conceptual/src/componant/Products/Products.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Product from '../product/product'
+ export default function Products () {
+
+  return (
+    <div>
+    <h2> I am from Products</h2>
+    </div>
+  )
+}
+
---
+++ React-recap-conceptual/react-recap-conceptual/src/componant/product/product.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+ export default function product () {
+  return (
+    <div>
+    <h2>This is from product 3rd child</h2>
+    </div>
+  )
+}
+
